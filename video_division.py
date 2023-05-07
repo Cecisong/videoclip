a0585b53d4464c42aa64e0bce42bec70367b9b57
Cecisong <130421504+Cecisong@users.noreply.github.com>
2023-05-07 22:31:47 +0800

    Update video_division.py

--- video_division.py
+++ video_division.py
@@ -17,25 +17,31 @@ import subprocess
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from datetime import timedelta
 
-video = VideoFileClip('G://test/BL/DJI_20230427195408_0075_D.MP4')
-# baseline
-# 滴滴声的时间字符串(标准时间已给定)
-# 问题：现在以眼动时间计算，实际发现滴滴声代表时间应该更晚
-mark_time = '2023-04-27 19:52:35.000'
-# 状态开始的时间字符串
-start_A2 = '2023-04-27 19:55:11.615974'
-# 开始截取的视频时间
+timedivision = pd.read_csv('G://P02/7.CANBus/timedivision--20230419160110.record.csv',
+                            usecols=[1, 5])
+didi_time = pd.read_excel('G://P02/5.RGBs/mark_timestamp.xlsx',usecols=[1])
+data_timedivision = timedivision.iloc[:, 0:2]
+data_timedivision = np.array(data_timedivision)
+didi = didi_time.iloc[:, :]
+didi = np.array(didi)
+start_time = data_timedivision[-12:,0]
+duration = data_timedivision[-12:,1]
+state = ['A1','A2','D1','A3','A4',
+         'L1','L2','R1','R2','R3','R4','L3']
+camera = ['BL', 'D1', 'D2', 'D3', 'P1', 'P2']
+file = ['Baseline', 'Driver1', 'Driver2', 'Driver3', 'Passenger1', 'Passenger2']
+mark_time = '2023-04-19 16:04:13.701'
 time1 = datetime.datetime.strptime(mark_time, "%Y-%m-%d %H:%M:%S.%f")
-time2 = datetime.datetime.strptime(start_A2, "%Y-%m-%d %H:%M:%S.%f")
-duration = (time2 - time1).total_seconds()
 
-video_start_A1 = duration + 8.61460317  # 滴滴声出现秒数
-
-# 结束截取的视频时间
-video_finish_A1 = video_start_A1 + 41.060841  # gps计算每一阶段所用时长
-
-clip = video.subclip(video_start_A1, video_finish_A1)
-clip.write_videofile('G://test/BL/PT-jiankong-BL-D1.mp4')
+for i in range(6):
+    video = VideoFileClip('G://P02/5.RGBs/' + file[i] + '/P02-' + camera[i] + '.mp4')
+    for j in range(len(duration)):
+        time2 = datetime.datetime.strptime(start_time[j], "%Y-%m-%d %H:%M:%S.%f")
+        relative_duration = (time2 - time1).total_seconds()
+        video_start = relative_duration + didi[i]
+        video_finish = video_start + duration[j]
+        clip = video.subclip(video_start, video_finish)
+        clip.write_videofile('G://P02/5.RGBs/clip/P02-'+ camera[i] + '-' + state[j] +'.mp4')
 
 
 
