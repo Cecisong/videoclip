c429a69472c039a0916fa3bf75a3fe76260f0098
Cecisong <130421504+Cecisong@users.noreply.github.com>
2023-05-22 19:58:49 +0800

    Update video_division.py

--- video_division.py
+++ video_division.py
@@ -1,25 +1,15 @@
-import os
-import ast
-import numpy as np
-import copy
-import calendar
-import time
-import argparse
-import json
-import logging
-import matplotlib.pyplot as plt
-import moviepy.editor
-import cv2
-import librosa
+import pandas as pd
 from envload import *
 import datetime
 import subprocess
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from datetime import timedelta
 
-timedivision = pd.read_csv('G://P02/7.CANBus/timedivision--20230419160110.record.csv',
+test_number = 'P12'
+
+timedivision = pd.read_csv('F://' + test_number+'/7.CANBus/timedivision--20230517160138.record.csv',
                             usecols=[1, 5])
-didi_time = pd.read_excel('G://P02/5.RGBs/mark_timestamp.xlsx',usecols=[1])
+didi_time = pd.read_excel('F://' + test_number+'/5.RGBs/mark_timestamp.xlsx',usecols=[1])
 data_timedivision = timedivision.iloc[:, 0:2]
 data_timedivision = np.array(data_timedivision)
 didi = didi_time.iloc[:, :]
@@ -30,18 +20,20 @@ state = ['A1','A2','D1','A3','A4',
          'L1','L2','R1','R2','R3','R4','L3']
 camera = ['BL', 'D1', 'D2', 'D3', 'P1', 'P2']
 file = ['Baseline', 'Driver1', 'Driver2', 'Driver3', 'Passenger1', 'Passenger2']
-mark_time = '2023-04-19 16:04:13.701'
+
+
+mark_time = '2023-05-17 16:09:23.633'
 time1 = datetime.datetime.strptime(mark_time, "%Y-%m-%d %H:%M:%S.%f")
 
 for i in range(6):
-    video = VideoFileClip('G://P02/5.RGBs/' + file[i] + '/P02-' + camera[i] + '.mp4')
+    video = VideoFileClip('F://' + test_number+'/5.RGBs/' + file[i] + '/' + test_number+'-' + camera[i] + '.mp4')
     for j in range(len(duration)):
         time2 = datetime.datetime.strptime(start_time[j], "%Y-%m-%d %H:%M:%S.%f")
         relative_duration = (time2 - time1).total_seconds()
         video_start = relative_duration + didi[i]
         video_finish = video_start + duration[j]
         clip = video.subclip(video_start, video_finish)
-        clip.write_videofile('G://P02/5.RGBs/clip/P02-'+ camera[i] + '-' + state[j] +'.mp4')
+        clip.write_videofile('F://' + test_number+'/5.RGBs/clip/' + test_number+'-'+ camera[i] + '-' + state[j] +'.mp4')
 
 
 
